84d0f9c711db756a906ef2c22c1bd653386d5df7
nurlanemir <nurlan.emiroff@gmail.com>
2017-11-09 14:29:11 +0100

    Add attribute of_type to a product serializer

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,3 +1,4 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :name, :price, :description, :image
+  attributes :id, :name, :price, :description, :image, :status
+  attribute :of_type, key: 'type'
 end
