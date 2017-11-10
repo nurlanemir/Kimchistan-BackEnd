f30970177f7b9c485a19f105e9c4c3147bc416b9
nurlanemir <nurlan.emiroff@gmail.com>
2017-11-10 10:13:22 +0100

    Update product controller, update product fixture file, tests are green

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,4 +1,4 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :name, :price, :description, :image, :status
+  attributes :id, :name, :price, :description, :image, :available
   attribute :of_type, key: 'type'
 end
