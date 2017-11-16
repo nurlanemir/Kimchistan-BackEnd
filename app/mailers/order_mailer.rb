92eda0d737fe00f8ebc717ce0fbca7e9ce5f3d4f
magnus-thor <magnus.thor.ar@gmail.com>
2017-11-16 13:58:19 +0100

    started on mailer

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,2 +1,9 @@
 class OrderMailer < ApplicationMailer
+
+  def order(user)
+    #TODO change when we know what have have to work with
+    @user = user
+    @url  = 'http://example.com/login'
+    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+  end
 end
