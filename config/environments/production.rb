5c0a33cf7f7401b75c639549f24331dbef7e4bb5
nurlanemir <nurlan.emiroff@gmail.com>
2018-01-03 17:48:27 +0100

    Add password in env file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,14 +3,14 @@ Rails.application.configure do
 
   config.eager_load = true
 
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
   config.read_encrypted_secrets = true
 
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.log_level = :debug
 
-  config.log_tags = [ :request_id ]
+  config.log_tags = [:request_id]
 
   config.action_mailer.perform_caching = false
 
@@ -23,19 +23,19 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-      address: ENV.fetch('SMTP_ADDRESS'),
-      authentication: ENV.fetch('SMTP_AUTH'),
-      domain: ENV.fetch('SMTP_DOMAIN'),
-      enable_starttls_auto: ENV.fetch('SMTP_ENABLE_TLS'),
-      password: ENV.fetch('SMTP_PASSWORD'),
-      port: ENV.fetch('SMTP_PORT'),
-      user_name: ENV.fetch('SMTP_USERNAME')
+      address: ENV['SMTP_ADDRESS'],
+      authentication: ENV['SMTP_AUTH'],
+      domain: ENV['SMTP_DOMAIN'],
+      enable_starttls_auto: ENV['SMTP_ENABLE_TLS'],
+      password: ENV['SMTP_PASSWORD'],
+      port: ENV['SMTP_PORT'],
+      user_name: ENV['SMTP_USERNAME']
   }
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
   config.active_record.dump_schema_after_migration = false
